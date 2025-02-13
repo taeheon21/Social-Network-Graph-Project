df8dccfe28f74fa79925b8a2aa6c89e1c004cac5
YaqinAlabbas <u5640968@live.warwick.ac.uk>
2025-02-13 14:24:21 +0000

    Update taeheoncode.py

--- taeheoncode.py
+++ taeheoncode.py
@@ -1,6 +1,10 @@
+import random
+import numpy as np
+
 class graph:
-    def __init__(self, directed = False):
+    def __init__(self,  size, directed = False):
         self.graph = {}
+        self.matrix = np.zeros ((size,size), dtype = int)
         self.directed = directed
         
     def add_member(self, member, age, interests):
@@ -15,14 +19,20 @@ class graph:
         else:
             print('Member does not exist')
     
-    def addrelations ( self, mem1, mem2, weight =1): #add edges
+    def addrelations ( self, mem1, mem2): #add edges
+        weight = random.randint(0, 10)
         if mem1 in self.graph and mem2 in self.graph:
             i,j = self.graph [mem1], self.graph[mem2]
             self.matrix [i][j] =weight
             self.matrix [j][i] =weight
         else:
             print ( "Both members must be initialized as objects to form a relationship")
-            
+
+    def removerrelation ( self, mem1, mem2):
+        if mem1 in self.graph and mem2 in self.graph:
+            i,j = self.graph [mem1], self.graph[mem2]
+            self.matrix [i][j] =0
+            self.matrix [j][i] =0    
         
 
 

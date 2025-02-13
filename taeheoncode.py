996e0d5e831dea43a4c29b5bc4fb38c69507aff7
codemaster04 <taeheon0121@gmail.com>
2025-02-13 14:43:25 +0000

    Update taeheoncode.py

--- taeheoncode.py
+++ taeheoncode.py
@@ -2,38 +2,40 @@ import random
 import numpy as np
 
 class graph:
-    def __init__(self, directed = False):
+    
     def __init__(self,  size, directed = False):
         self.graph = {}
         self.matrix = np.zeros ((size,size), dtype = int)
         self.directed = directed
         
     def add_member(self, member, age, interests):
-
-@@ -15,14 +19,20 @@ class graph:
+        if member not in self.graph:
+             self.graph[member] = {'age': age, 'interests': interests, 'connections': {}}
+        else:
+            print('Member already exists')
+            
+     def remove_member(self, member):
+        if member in self.graph:
+            del self.graph[member]
         else:
             print('Member does not exist')
-            print("t")
-    def addrelations ( self, mem1, mem2): #add edges
+
+
+    
+    def add_relations ( self, mem1, mem2): #add edges
         weight = random.randint(0, 10)
         if mem1 in self.graph and mem2 in self.graph:
             i,j = self.graph [mem1], self.graph[mem2]
             self.matrix [i][j] =weight
             self.matrix [j][i] =weight
         else:
-<<<<<<< Updated upstream
-            print ( "Both members must be initilised as objects to form a relationship")
+        print ( "Both members must be initilised as objects to form a relationship")
 
-=======
-            print ( "Both members must be initialized as objects to form a relationship")
-            
->>>>>>> Stashed changes
 
-    def removerrelation ( self, mem1, mem2):
+    def remove_rrelation ( self, mem1, mem2):
         if mem1 in self.graph and mem2 in self.graph:
             i,j = self.graph [mem1], self.graph[mem2]
             self.matrix [i][j] =0
-<<<<<<< Updated upstream
             self.matrix [j][i] =0        
     
     def find_friends(self, memeber):
@@ -92,9 +94,6 @@ print("Mutual friends between Taeheon and Sultan:", network.find_mutual_friends(
 
          
        
-=======
-            self.matrix [j][i] =0     
->>>>>>> Stashed changes
         
         
         

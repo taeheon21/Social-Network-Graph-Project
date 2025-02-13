3840007a7d19538bb6d9d088a9513b63c81fb7fa
YaqinAlabbas <u5640968@live.warwick.ac.uk>
2025-02-13 14:35:10 +0000

    Update taeheoncode.py

--- taeheoncode.py
+++ taeheoncode.py
@@ -2,20 +2,15 @@ import random
 import numpy as np
 
 class graph:
+    def __init__(self, directed = False):
     def __init__(self,  size, directed = False):
         self.graph = {}
         self.matrix = np.zeros ((size,size), dtype = int)
         self.directed = directed
         
     def add_member(self, member, age, interests):
-        if member not in self.graph:
-            self.graph[member] = {'age': age, 'interests': interests}
-        else:
-            print('Member already exists')
-            
-    def remove_member(self, member):
-        if member in self.graph:
-            del self.graph[member]
+
+@@ -15,14 +19,20 @@ class graph:
         else:
             print('Member does not exist')
             print("t")
@@ -26,13 +21,19 @@ class graph:
             self.matrix [i][j] =weight
             self.matrix [j][i] =weight
         else:
+<<<<<<< Updated upstream
             print ( "Both members must be initilised as objects to form a relationship")
 
+=======
+            print ( "Both members must be initialized as objects to form a relationship")
+            
+>>>>>>> Stashed changes
 
     def removerrelation ( self, mem1, mem2):
         if mem1 in self.graph and mem2 in self.graph:
             i,j = self.graph [mem1], self.graph[mem2]
             self.matrix [i][j] =0
+<<<<<<< Updated upstream
             self.matrix [j][i] =0        
     
     def find_friends(self, memeber):
@@ -91,6 +92,9 @@ print("Mutual friends between Taeheon and Sultan:", network.find_mutual_friends(
 
          
        
+=======
+            self.matrix [j][i] =0     
+>>>>>>> Stashed changes
         
         
         
